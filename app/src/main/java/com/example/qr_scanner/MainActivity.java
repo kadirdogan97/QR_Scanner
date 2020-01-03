6449a28d53cccd1dd41cf2640029cc63dcbcfebf
WSRising <wsrising@gmail.com>
2020-01-03 19:57:05 +0300

    first commit

---
+++ app/src/main/java/com/example/qr_scanner/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.qr_scanner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static TextView resultTextView;
+    private Button scanButton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        resultTextView = findViewById(R.id.textView);
+        scanButton = findViewById(R.id.button);
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), ScanCodeActivity.class));
+            }
+        });
+
+    }
+}
